834e0c31815d539999c685ab47360060d4bf06f2
alaguselvan <alaguselvan97@gmail.com>
2021-07-30 21:43:57 +0530

    structure modified

---
+++ concepts/hoisting.js
@@ -0,0 +1,25 @@
+var i = 0;
+
+
+callOne();
+callTwo();
+
+
+function callOne() {
+    var i = 1; 
+    console.log(i);
+}
+
+function callTwo() {
+    var i = 2;
+    console.log(i);
+}
+
+
+const callThree = () => {
+    var i = 3;
+    console.log(i);
+}
+
+
+console.log(i);
