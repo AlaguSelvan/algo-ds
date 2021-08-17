cd1200a55a34317dbbb68e7a25cf36f0b18bbcf2
Alagu Selvan <alaguselvan97@gmail.com>
2021-08-17 23:03:25 +0530

    pair sums added

--- Algorithms/arrays/PairSums.js
+++ Algorithms/arrays/PairSums.js
@@ -11,13 +11,14 @@ function numberOfWays(arr, k) {
 		arr.map(ar => hashMap[ar] ? hashMap[ar]++ : hashMap[ar] = 1);
 		arr.map(current => {
 			let targetsum = k - current;
-			if(targetsum in hashMap && hashMap[targetsum] > 0) {
-				hashMap[current]--
-				count += 1
+			if(targetsum in hashMap) {
+				count += hashMap[targetsum]
+			 }
+			 if(targetsum === current) {
+				count--;
 			 }
 		})
-		console.log(count);
-		return count
+		return count / 2;
 }
 
 
@@ -64,10 +65,10 @@ var expected_1 = 2;
 var output_1 = numberOfWays(arr_1, k_1);
 check(expected_1, output_1);
 
-// var k_2 = 6;
-// var arr_2 = [1, 5, 3, 3, 3];
-// var expected_2 = 4;
-// var output_2 = numberOfWays(arr_2, k_2);
-// check(expected_2, output_2);
+var k_2 = 6;
+var arr_2 = [1, 5, 3, 3, 3];
+var expected_2 = 4;
+var output_2 = numberOfWays(arr_2, k_2);
+check(expected_2, output_2);
 
 // Add your own test cases here
