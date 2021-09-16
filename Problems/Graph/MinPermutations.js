a03ab1dc13534042566b91317639b31d6548b237
Alagu Selvan <alaguselvan97@gmail.com>
2021-09-16 20:48:33 +0530

    few changes

--- Problems/Graph/MinPermutations.js
+++ Problems/Graph/MinPermutations.js
@@ -2,29 +2,7 @@
 
 
 // Add any helper functions you may need here
-// def minOperations(arr):
 
-//     n = 0
-    
-//     for i in range(len(arr)): # assign i every value in the array
-  
-//     # i is the value, j is the list position
-//     #using .index() we can return the position of a value
-      
-//     #Essentially, we are assigning the value and position to seperate objects (i, j).
-//         j = arr.index(min(arr[i:])) # j will store the current position of the value we are going to swith
-    
-//         if (j != i): #if the current position and current value do no match then
-        
-//         #this is the fascinating part of this algorithm:
-//             arr = arr[:i] + [ v for v in reversed( arr[i:j+1] ) ] + arr[ j+1: ]
-        
-//         #a nice way to visually see what is happening:
-//             print ("Examine: (%d,%d) = %s" % (i, j, arr)) 
-//             n += 1
-      
-//     print (n)
-//     return n
 
 
 function minOperations(arr) {
